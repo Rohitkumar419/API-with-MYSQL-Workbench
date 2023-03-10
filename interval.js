afb5d106889640ef4772acfec8abad630b82569d
rohisahu@deloitte.com <rohisahu@deloitte.com>
2023-03-10 10:30:41 +0530

    setTimeOut( ) & setInterval( )

---
+++ interval.js
@@ -0,0 +1,14 @@
+//SetInterval function
+function intervalFunc() {
+    console.log('Complete Assignment!');
+  }
+  
+  setInterval(intervalFunc,1000);
+
+
+
+  //setTimeOut function
+function timeoutfunc() {
+    console.log('This is a demo')
+}
+setTimeout(timeoutfunc, 2000)
